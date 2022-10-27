32840ef1b78b276dd1bb78d484221f53bf7eb5ef
Flavius <flavius.popa123@gmail.com>
2022-10-27 12:48:15 +0300

    still have to solve the problem where the consol says that can't read the property of null on the line 43 of the js script

--- script.js
+++ script.js
@@ -1,12 +1,54 @@
 const selectionButtons = document.querySelectorAll('[data-selection]');
+const finalColumn = document.querySelector('data-final-column')
+
+const SELECTIONS = [
+{   
+    name: 'rock',
+    emoji: '✊',
+    beats: 'scissors'
+},
+{
+    name: 'paper',
+    emoji: '✋',
+    beats: 'rock'  
+},
+{
+    name: 'scissors',
+    emoji: '✌',
+    beats: 'paper'  
+}]
 
 selectionButtons.forEach(selectionButton => {
     selectionButton.addEventListener('click', e => {
-      const selectionName = selectionButton.dataset.selection;
-      makeSelection(selectionName)
+      const selectionName = selectionButton.dataset.selection
+      const selection = SELECTIONS.find(selection => selection.name === selectionName)
+      makeSelection(selection)
     })
   })
   
-function makeSelection(selection){
-    console.log(selection);
+function makeSelection(selection) {
+    const computerSelection = randomSelection();
+    const yourWinner = isWinner(selection, computerSelection);
+    const computerWinner = isWinner(computerSelection, selection);
+    
+    addSelectionResult(computerSelection, computerWinner);
+    addSelectionResult(selection, yourWinner);
+   
+}
+function addSelectionResult(selection, winner) {
+    const div= document.createElement('div')
+    div.innerText = selection.emoji
+    div.classList.add('result-selection')
+    if (winner) div.classList.add('winner')
+    finalColumn.after(div)
+  }
+  
+
+function isWinner(selection, opponentSelection){
+    return selection.beats === opponentSelection.name
+}
+
+function randomSelection(){
+    const randomIndex = Math.floor(Math.random() * SELECTIONS.length);
+    return SELECTIONS[randomIndex]
 }
